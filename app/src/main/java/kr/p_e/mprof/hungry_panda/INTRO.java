b805887789d18992c14e17379e2a10e27dc65926
lbm99999 <lbm0813@gmail.com>
2017-06-03 20:26:53 +0900

    INTRO Fix #1 INTRO Time 3secs #2 INTRO Time 0.0001sec add #3

--- app/src/main/java/kr/p_e/mprof/hungry_panda/INTRO.java
+++ app/src/main/java/kr/p_e/mprof/hungry_panda/INTRO.java
@@ -25,6 +25,6 @@ public class INTRO extends AppCompatActivity {
                     startActivity(intent);
                     finish();
                 }
-            }, 3000);
+            }, 3001);
     }
 }
